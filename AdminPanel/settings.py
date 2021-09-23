222fc40b6bcf3fc8b936dee2c8e08b0fbb5aa182
Polarts <polarbear.oc@gmail.com>
2021-09-23 12:13:51 +0300

    Added heroku setup

--- AdminPanel/settings.py
+++ AdminPanel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,7 @@ STATICFILES_DIRS = ( os.path.join(STATIC_ROOT, 'AdminPanel'), )
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+
+django_heroku.settings(locals())
